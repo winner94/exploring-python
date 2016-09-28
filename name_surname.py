8970deb34d04bfde16265a43bb4a2e4d627a3c7a
winner94 <tkolecki@gmail.com>
2016-09-28 23:06:41 +0200

    Add files via upload

---
+++ name_surname.py
@@ -0,0 +1,5 @@
+text = input('Give me your name and surname: ')
+name = text.split()
+
+print('Your name is: ', name[0])
+print('Your surname is: ', name[1])
---
+++ square-root.py
@@ -0,0 +1,4 @@
+from math import sqrt
+
+a = int(input('Input value > '))
+print(sqrt(a))
